df419e39e8df778c76385b7f426afbb946f74239
Sonia Hamilton <sonia@snowfrog.net>
2015-08-07 10:43:56 +1000

    flags: no -p; -f, -v

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -12,7 +11,11 @@ import (
 )
 
 var opts struct {
-	Profile string `short:"p" long:"profile" description:"profile to use" required:"true"`
+	Positional struct {
+		Profile string
+	} `positional-args:"yes" required:"yes"`
+	Verbose  bool   `short:"v" long:"verbose" description:"Verbose output"`
+	Filename string `short:"f" long:"filename" description:"Credentials file" default:"~/.aws/credentials"`
 }
 
 // expandPath expands '~' in path
@@ -28,20 +31,26 @@ func main() {
 	// parse args
 	_, err := flags.Parse(&opts)
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	// load credentials
-	credentials := filepath.Join(os.Getenv("HOME"), "/.aws/credentials")
-	cfg, err := ini.Load(credentials)
-	if err != nil {
-		log.Fatal(err)
+		typ := err.(*flags.Error).Type
+		if typ == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 
 	// load from profile
-	section, err := cfg.GetSection(opts.Profile)
+	credentials_path := expandPath(opts.Filename)
+	cfg, err := ini.Load(credentials_path)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	section, err := cfg.GetSection(opts.Positional.Profile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	// AWS_KEY
@@ -61,12 +70,17 @@ func main() {
 	if len(keyname) > 0 {
 		out += fmt.Sprintf("export AWS_KEYNAME='%s'; ", keyname)
 	} else {
-		out += fmt.Sprintf("unset AWS_KEYNAME; ")
+		out += "unset AWS_KEYNAME; "
 	}
 	if len(keypath) > 0 {
 		out += fmt.Sprintf("export AWS_KEYPATH='%s'; ", keypath)
 	} else {
-		out += fmt.Sprintf("unset AWS_KEYPATH; ")
+		out += "unset AWS_KEYPATH; "
+	}
+
+	// verbose?
+	if opts.Verbose {
+		out += "env | grep AWS; "
 	}
 
 	fmt.Println(out)
